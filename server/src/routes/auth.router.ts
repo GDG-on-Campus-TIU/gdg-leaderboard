e7ab0cf0216b235259f261bab2cac171d9bbcb34
devpiush <dev.bosepiush@gmail.com>
2025-03-29 23:46:09 +0530

    code: working on auth with prisma

--- server/src/routes/auth.router.ts
+++ server/src/routes/auth.router.ts
@@ -1,5 +1,29 @@
-import { Hono } from "hono";
+import { Hono, Context } from "hono";
+import { sign } from "hono/jwt"
 
 const authRouter = new Hono();
+
+authRouter.post("/login", async (c: Context) => {
+  const jwt_payload = c.get("jwt_payload")
+
+  if (jwt_payload.user_details) {
+    // TODO: make changes to refresh the expiry of the token here
+    c.json({
+      message: "Already logged in!"
+    }, 201)
+    return
+  }
+
+  const { email, password } = await c.req.json<{ email: string; password: string; }>()
+
+  if (!email || !password) {
+    c.json({
+      message: "Missing required field, email and password must be sent!"
+    }, 400)
+    return
+  }
+
+
+})
 
 export { authRouter };
