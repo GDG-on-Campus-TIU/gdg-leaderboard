fb772684bfd9095a26526fa4b12cca0287486f9d
Ankit Seal <ankitseal2021@gmail.com>
2025-04-01 01:53:46 +0530

    glass morphism done

--- client/app.js
+++ client/app.js
@@ -1,5 +1,67 @@
 import express from "express";
 import content from "./src/content.js";
+import { aiml_player_details } from "./src/data.js";
+import { dsa_player_details } from "./src/data.js";
+import { cloud_player_details } from "./src/data.js";
+import { webd_player_details } from "./src/data.js";
+function getAIMLLeaderboard(){
+    return `
+            ${aiml_player_details.map((player)=>{
+            return `
+                <tr>
+                    <td class="td">${player.rank}</td>
+                    <td class="td">${player.name}</td>
+                    <td class="td">${player.attendance}</td>
+                    <td class="td">${player.participation}</td>
+                    <td class="td">${player.projects}</td>
+                    <td class="td">${player.total}</td>
+                </tr>`}).join("")}
+           `;
+}
+
+function getDSALeaderboard(){
+    return `
+            ${dsa_player_details.map((player)=>{
+            return `
+                <tr>
+                    <td class="td">${player.rank}</td>
+                    <td class="td">${player.name}</td>
+                    <td class="td">${player.attendance}</td>
+                    <td class="td">${player.participation}</td>
+                    <td class="td">${player.projects}</td>
+                    <td class="td">${player.total}</td>
+                </tr>`}).join("")}
+           `;
+}
+function getCloudLeaderboard(){
+    return `
+            ${cloud_player_details.map((player)=>{
+            return `
+                <tr>
+                    <td class="td">${player.rank}</td>
+                    <td class="td">${player.name}</td>
+                    <td class="td">${player.attendance}</td>
+                    <td class="td">${player.participation}</td>
+                    <td class="td">${player.projects}</td>
+                    <td class="td">${player.total}</td>
+                </tr>`}).join("")}
+           `;
+}
+function getWebDLeaderboard(){  
+    return `
+            ${webd_player_details.map((player)=>{
+            return `
+                <tr>
+                    <td class="td">${player.rank}</td>
+                    <td class="td">${player.name}</td>
+                    <td class="td">${player.attendance}</td>
+                    <td class="td">${player.participation}</td>
+                    <td class="td">${player.projects}</td>
+                    <td class="td">${player.total}</td>
+                </tr>`}).join("")}
+           `;
+}
+
 const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('public/'));
@@ -8,8 +70,19 @@ app.use(express.static('public/styles/'));
 app.get("/", (req,res)=>{
     res.send(content());
 })
-
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+app.get("/aiml-leaderboard", (req,res)=>{
+    res.send(getAIMLLeaderboard());
+})
+app.get("/dsa-leaderboard", (req,res)=>{
+    res.send(getDSALeaderboard());
+})
+app.get("/cloud-leaderboard", (req,res)=>{
+    res.send(getCloudLeaderboard());
+})
+app.get("/webd-leaderboard", (req,res)=>{
+    res.send(getWebDLeaderboard());
+})
+app.listen(3001, () => {
+    console.log("Server is running on http://localhost:3001");
 }
 );
--- client/src/data.js
+++ client/src/data.js
@@ -1,4 +1,4 @@
-let player_details=[
+export let player_details=[
     {
         "rank": 1,
         "name": "Player 1",
@@ -80,6 +80,334 @@ let player_details=[
         "total": 26
     }
 ]
+export let aiml_player_details=[
+    {
+        "rank": 1,
+        "name": "AIML_Player1",
+        "attendance": 9,
+        "participation": 7,
+        "projects": 80,
+        "total": 96
+    },
+    {
+        "rank": 2,
+        "name": "AIML_Player2",
+        "attendance": 4,
+        "participation": 5,
+        "projects": 50,
+        "total": 59
+    },
+    {
+        "rank": 3,
+        "name": "AIML_Player3",
+        "attendance": 6,
+        "participation": 8,
+        "projects": 72,
+        "total": 86
+    },
+    {
+        "rank": 4,
+        "name": "AIML_Player4",
+        "attendance": 5,
+        "participation": 3,
+        "projects": 40,
+        "total": 48
+    },
+    {
+        "rank": 5,
+        "name": "AIML_Player5",
+        "attendance": 2,
+        "participation": 6,
+        "projects": 66,
+        "total": 74
+    },
+    {
+        "rank": 6,
+        "name": "AIML_Player6",
+        "attendance": 7,
+        "participation": 9,
+        "projects": 85,
+        "total": 101
+    },
+    {
+        "rank": 7,
+        "name": "AIML_Player7",
+        "attendance": 3,
+        "participation": 4,
+        "projects": 33,
+        "total": 40
+    },
+    {
+        "rank": 8,
+        "name": "AIML_Player8",
+        "attendance": 10,
+        "participation": 2,
+        "projects": 55,
+        "total": 67
+    },
+    {
+        "rank": 9,
+        "name": "AIML_Player9",
+        "attendance": 1,
+        "participation": 10,
+        "projects": 29,
+        "total": 40
+    },
+    {
+        "rank": 10,
+        "name": "AIML_Player10",
+        "attendance": 8,
+        "participation": 1,
+        "projects": 47,
+        "total": 56
+    }
+];
 
+export let dsa_player_details = [
+    {
+        "rank": 1,
+        "name": "DSA_Player1",
+        "attendance": 8,
+        "participation": 9,
+        "projects": 90,
+        "total": 107
+    },
+    {
+        "rank": 2,
+        "name": "DSA_Player2",
+        "attendance": 3,
+        "participation": 6,
+        "projects": 54,
+        "total": 63
+    },
+    {
+        "rank": 3,
+        "name": "DSA_Player3",
+        "attendance": 7,
+        "participation": 8,
+        "projects": 78,
+        "total": 93
+    },
+    {
+        "rank": 4,
+        "name": "DSA_Player4",
+        "attendance": 5,
+        "participation": 4,
+        "projects": 45,
+        "total": 54
+    },
+    {
+        "rank": 5,
+        "name": "DSA_Player5",
+        "attendance": 4,
+        "participation": 7,
+        "projects": 62,
+        "total": 73
+    },
+    {
+        "rank": 6,
+        "name": "DSA_Player6",
+        "attendance": 9,
+        "participation": 10,
+        "projects": 88,
+        "total": 107
+    },
+    {
+        "rank": 7,
+        "name": "DSA_Player7",
+        "attendance": 2,
+        "participation": 5,
+        "projects": 39,
+        "total": 46
+    },
+    {
+        "rank": 8,
+        "name": "DSA_Player8",
+        "attendance": 6,
+        "participation": 3,
+        "projects": 51,
+        "total": 60
+    },
+    {
+        "rank": 9,
+        "name": "DSA_Player9",
+        "attendance": 1,
+        "participation": 8,
+        "projects": 36,
+        "total": 45
+    },
+    {
+        "rank": 10,
+        "name": "DSA_Player10",
+        "attendance": 10,
+        "participation": 2,
+        "projects": 49,
+        "total": 61
+    }
+];
 
-export default player_details;
+export let webd_player_details = [
+    {
+        "rank": 1,
+        "name": "Webd_Player1",
+        "attendance": 9,
+        "participation": 6,
+        "projects": 77,
+        "total": 92
+    },
+    {
+        "rank": 2,
+        "name": "Webd_Player2",
+        "attendance": 5,
+        "participation": 5,
+        "projects": 60,
+        "total": 70
+    },
+    {
+        "rank": 3,
+        "name": "Webd_Player3",
+        "attendance": 7,
+        "participation": 7,
+        "projects": 82,
+        "total": 96
+    },
+    {
+        "rank": 4,
+        "name": "Webd_Player4",
+        "attendance": 3,
+        "participation": 4,
+        "projects": 42,
+        "total": 49
+    },
+    {
+        "rank": 5,
+        "name": "Webd_Player5",
+        "attendance": 6,
+        "participation": 8,
+        "projects": 68,
+        "total": 82
+    },
+    {
+        "rank": 6,
+        "name": "Webd_Player6",
+        "attendance": 10,
+        "participation": 9,
+        "projects": 89,
+        "total": 108
+    },
+    {
+        "rank": 7,
+        "name": "Webd_Player7",
+        "attendance": 2,
+        "participation": 5,
+        "projects": 38,
+        "total": 45
+    },
+    {
+        "rank": 8,
+        "name": "Webd_Player8",
+        "attendance": 4,
+        "participation": 3,
+        "projects": 47,
+        "total": 54
+    },
+    {
+        "rank": 9,
+        "name": "Webd_Player9",
+        "attendance": 1,
+        "participation": 10,
+        "projects": 31,
+        "total": 42
+    },
+    {
+        "rank": 10,
+        "name": "Webd_Player10",
+        "attendance": 8,
+        "participation": 1,
+        "projects": 55,
+        "total": 64
+    }
+];
+
+export let cloud_player_details = [
+    {
+        "rank": 1,
+        "name": "Cloud_Player1",
+        "attendance": 8,
+        "participation": 10,
+        "projects": 75,
+        "total": 93
+    },
+    {
+        "rank": 2,
+        "name": "Cloud_Player2",
+        "attendance": 6,
+        "participation": 7,
+        "projects": 63,
+        "total": 76
+    },
+    {
+        "rank": 3,
+        "name": "Cloud_Player3",
+        "attendance": 9,
+        "participation": 5,
+        "projects": 81,
+        "total": 95
+    },
+    {
+        "rank": 4,
+        "name": "Cloud_Player4",
+        "attendance": 5,
+        "participation": 4,
+        "projects": 48,
+        "total": 57
+    },
+    {
+        "rank": 5,
+        "name": "Cloud_Player5",
+        "attendance": 7,
+        "participation": 8,
+        "projects": 71,
+        "total": 86
+    },
+    {
+        "rank": 6,
+        "name": "Cloud_Player6",
+        "attendance": 10,
+        "participation": 9,
+        "projects": 85,
+        "total": 104
+    },
+    {
+        "rank": 7,
+        "name": "Cloud_Player7",
+        "attendance": 4,
+        "participation": 6,
+        "projects": 52,
+        "total": 62
+    },
+    {
+        "rank": 8,
+        "name": "Cloud_Player8",
+        "attendance": 2,
+        "participation": 3,
+        "projects": 34,
+        "total": 39
+    },
+    {
+        "rank": 9,
+        "name": "Cloud_Player9",
+        "attendance": 1,
+        "participation": 7,
+        "projects": 28,
+        "total": 36
+    },
+    {
+        "rank": 10,
+        "name": "Cloud_Player10",
+        "attendance": 3,
+        "participation": 1,
+        "projects": 40,
+        "total": 44
+    }
+];
