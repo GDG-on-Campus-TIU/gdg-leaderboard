d4532ba5ed0f99517102cfbfb142cdd0a672ae6b
devpiush <dev.bosepiush@gmail.com>
2025-04-13 21:40:45 +0530

    code: edited the cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -36,7 +36,7 @@ app.use(
 app.use(
   "*",
   cors({
-    origin: ["*", "localhost:3001"],
+    origin: ["*", "localhost:3001", "gdgtiu.org"],
     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
