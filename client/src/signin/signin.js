00e78477e0b86f5310797ca5bc27621b94b73656
Ankit Seal <ankitseal2021@gmail.com>
2025-04-14 22:37:47 +0530

    sign-up/in done. signin request setup to be done

--- client/src/signin/signin.js
+++ client/src/signin/signin.js
@@ -6,47 +6,51 @@ let signin = () =>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Document</title>
-                <link rel="stylesheet" href="/styles/signin/styles.css"> 
                 <link rel="stylesheet" href="/styles/signin/output.css">   
+                <link rel="stylesheet" href="/styles/signin/styles.css"> 
                 <script src="/scripts/script-signin.js" ></script>
             </head>
-            <h1>SIGN IN TO GDG</h1>
+            <h1 class="main-heading">SIGN IN TO GDG</h1>
             <div class="container right-panel-active" id="container" > 
                 <!-- //TODO: remove right-panel-active default class -->
-                <div class="form-container sign-up-container">
-                    <form id="signupForm" method="POST">
-                        <h1>Create Account</h1>
-                        <div class="pfp-wrapper">
-                            <input
-                                type="file" 
-                                class="pfp-input" 
-                                id="fileInput" onchange="pfp_check()"
-                                accept=".png, .jpg, .jpeg"
-                                >
-                        </div>
-                        <label for="fileInput" class="pfp-input-label">Choose an image, or drag it here
-                            <sub class=" flex justify-center">only .jpg/.jpeg/.png is accepted</sub>
-                        </label>
-                        <input type="text" placeholder="Name" />
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <button>Sign Up</button>
-                    </form>
-                </div>
-                <div class="form-container sign-in-container">
-                    <form action="#">
-                        <h1>Sign in</h1>
-                        <div class="social-container">
-                            <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                            <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-                        </div>
-                        <span>or use your account</span>
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <a href="#">Forgot your password?</a>
-                        <button>Sign In</button>
-                    </form>
+                <div class="mobile-view-container" >
+                    <div class="form-container sign-up-container">
+                        <form id="signupForm" method="POST">
+                            <h1>Create Account</h1>
+                            <div class="pfp-wrapper">
+                                <input
+                                    name="pfp"
+                                    type="file" 
+                                    class="pfp-input" 
+                                    id="fileInput" onchange="pfp_check()"
+                                    accept=".png, .jpg, .jpeg"
+                                    >
+                            </div>
+                            <label for="fileInput" class="pfp-input-label">Upload a <i>1:1</i> image for best results, or drag it here
+                                <div class=" flex justify-center sub-text">only .jpg/.jpeg/.png is accepted</div>
+                            </label>
+                            <div class="input-collection">
+                            <div class="input-line1">
+                                <input type="text" name="name" placeholder="Name" />
+                                <input type="text" name="clgId" placeholder="College ID" />
+                            </div>   
+                            <div class="input-line2">
+                                <input type="email" name="email" placeholder="Email" />
+                                <input type="password" name="password" placeholder="Password" />
+                            </div> 
+                            </div>
+                            <button onclick="handleSignup()">Sign Up</button>
+                        </form>
+                    </div>
+                    <div class="form-container sign-in-container">
+                        <form action="#">
+                            <h1>Sign in</h1>
+                            <input type="email" placeholder="Email" />
+                            <input type="password" placeholder="Password" />
+                            <a href="#">Forgot your password?</a>
+                            <button class="sign-in-button">Sign In</button>
+                        </form>
+                    </div>
                 </div>
                 <div class="overlay-container">
                     <div class="overlay">
@@ -63,37 +67,6 @@ let signin = () =>
                     </div>
                 </div>
             </div>
-            <script>
-
-                const form = document.getElementById('signupForm');
-                form.addEventListener('submit', async function (e) 
-                {
-                    e.preventDefault();
-                    // Extract data for JSON
-                    const formData = new FormData(form);
-                    const jsonData = {
-                        name: formData.get('name'),
-                        email: formData.get('email'),
-                        password: formData.get('password'),
-                        clgId: formData.get('clgId')
-                    };
-                    const signupRes = await fetch ('https://gdg-leaderboard-server-1019775793519.us-central1.run.app/api/v1', {
-                        method: "POST",
-                        headers:{
-                            'Content-Type': 'application/json',
-                            'HX-Request': 'true'
-                        }
-                        body: JSON.stringify(jsonData)
-                    });
-                    if (!signupRes.ok) {
-                        console.log('Signup failed!');
-                        return;
-                    }
-                    else{
-                        console.log("signup done")
-                    }
-                })
-            </script>
         </html>
     `
 export default signin;
